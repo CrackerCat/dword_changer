6dbeedfd2788f6c05a3c4d8ae6a232168fa01161
Hyun Yi <hy00un__@naver.com>
2018-08-15 09:32:21 +0900

    Upload

--- dword_changer.py
+++ dword_changer.py
@@ -10,6 +10,14 @@ except:
 
 from functools import partial
 
+import base64
+from Crypto.Cipher import ARC4
+def decrypt_string(string):
+    string = base64.b64decode(string)
+    cipher = ARC4.new("4s3h8j2q")
+    ''' You should make decrypt code'''
+    return cipher.decrypt(string)
+
 string = []
 dword_addr = []
 blacklist_string = ["!", "#", "^", "*", "-", "+", "=", ";", "'", "\"", "\\", "|", "{", "}", "/", "<", ">", ","]
@@ -22,8 +30,6 @@ def get_string(addr):
         else:
             break
         addr += 1
-    for i in blacklist_string:
-        out = out.replace(i, "_")
     return out
 
 class dword_changer(PluginForm):
@@ -40,12 +46,18 @@ class dword_changer(PluginForm):
             if "offset" in GetOpnd(start, 0):
                 variable = GetOpnd(start, 0).split(" ")[1]
                 addr = get_name_ea(start,variable)
-                string.append(get_string(addr))
+                result = decrypt_string(get_string(addr))
+                for i in blacklist_string:
+                    result = result.replace(i, "_")
+                string.append(result)
 
             elif "offset" in GetOpnd(start, 1):
                 variable = GetOpnd(start, 1).split(" ")[1]
                 addr = get_name_ea(start,variable)
-                string.append(get_string(addr))
+                result = decrypt_string(get_string(addr))
+                for i in blacklist_string:
+                    result = result.replace(i, "_")
+                string.append(result)
 
             elif "dword_" in GetOpnd(start, 0) and GetMnem(start) == "mov":
                 dword_addr.append(GetOpnd(start, 0))
@@ -59,6 +71,9 @@ class dword_changer(PluginForm):
             print dword_addr[i], string[i] + "_" + dword_addr[i]
             idc.MakeName(int(dword_addr[i].replace("dword_",""),16), string[i] + "_" + dword_addr[i])
 
+    def save_func(self):
+        pass
+
     def OnCreate(self, form):
         try:
             self.parent = self.FormToPyQtWidget(form)
@@ -71,6 +86,9 @@ class dword_changer(PluginForm):
         self.EndAddress = QLineEdit()
         self.PushButton1 = QPushButton("Change")
         self.PushButton1.clicked.connect(self.rename_variable)
+        self.TextEdit1 = QPlainTextEdit()
+        self.PushButton2 = QPushButton("Save")
+        self.PushButton2.clicked.connect(self.save_func)
 
         self.layout = QVBoxLayout()
         GL1 = QGridLayout()
@@ -83,14 +101,16 @@ class dword_changer(PluginForm):
         GL2 = QGridLayout()
         GL2.addWidget(self.PushButton1, 0, 0)
         self.layout.addLayout(GL2)
+
+        GL3 = QGridLayout()
+        GL3.addWidget(self.TextEdit1, 0, 0)
+        GL3.addWidget(self.PushButton2, 1, 0)
+        self.layout.addLayout(GL3)
         self.parent.setLayout(self.layout)
 
     def OnClose(self, form):
         pass
 
-plg = dword_changer()
-plg.Show("dword_changer")
-'''
 class Plugin(idaapi.plugin_t):
     flags = idaapi.PLUGIN_UNL
     comment = "dword_changer"
@@ -111,4 +131,3 @@ class Plugin(idaapi.plugin_t):
 
 def PLUGIN_ENTRY():
     return Plugin()
-'''
